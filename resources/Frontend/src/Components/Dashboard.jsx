756a3938f1db7bd5bca72ee1af39595cd1593514
Supun Nethsara <supunmax663@gmail.com>
2025-04-25 00:40:42 -0700

    Tailwind css Installed

--- resources/Frontend/src/Components/Dashboard.jsx
+++ resources/Frontend/src/Components/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Dashboard() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+        </div>
+    );
 }
