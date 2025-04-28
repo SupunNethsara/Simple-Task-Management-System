5c178e3f652614e27278ccd053479b85933452ed
Supun Nethsara <supunmax663@gmail.com>
2025-04-28 01:12:20 -0700

    Search Bar Location is Changed in Dashboard

--- resources/Frontend/src/Components/Dashboard.jsx
+++ resources/Frontend/src/Components/Dashboard.jsx
@@ -41,7 +41,10 @@ export default function Dashboard() {
                         </button>
 
 
-                        <div className="relative flex-1 max-w-md ml-10">
+                       
+                    </div>
+                    <div className="flex items-center space-x-4">
+                    <div className="relative flex-1 max-w-md ml-10">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <FiSearch className="h-5 w-5 text-gray-400" />
                             </div>
@@ -51,8 +54,6 @@ export default function Dashboard() {
                                 className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                             />
                         </div>
-                    </div>
-                    <div className="flex items-center space-x-4">
                         <button className="p-1 rounded-full text-gray-500 hover:text-gray-700 hover:bg-gray-100">
                             <FiBell className="h-6 w-6" />
                         </button>
